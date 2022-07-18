6951b199878a847428e45f700e69d50e9f325dc2
pilar sanchez <mpilar-93@hotmail.com>
2022-07-18 18:47:50 +0100

    added data file

--- meme_generator/meme-generator/src/components/Meme.js
+++ meme_generator/meme-generator/src/components/Meme.js
@@ -1,13 +1,21 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
-    return (
-      <main>
-        <form>
-          <input className="form--input" type="text" placeholder="Top text"/>
-          <input className="form--input" type="text" placeholder="Bottom text"/>
-          <button className="form--button">Get a new meme image 🔥</button>
-        </form>
-      </main>
-    )
+
+  function getMemeImage() {
+    const memes = memesData.data.memes
+    let num = Math.floor(Math.random() * memes.length + 1)
+    console.log(memes[num].url)
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input className="form--input" type="text" placeholder="Top text"/>
+        <input className="form--input" type="text" placeholder="Bottom text"/>
+        <button onClick={getMemeImage} className="form--button">Get a new meme image 🔥</button>
+      </div>
+    </main>
+  )
 }
